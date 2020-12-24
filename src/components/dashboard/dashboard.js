0772388db54651cf27dfee460cafe89ff075b801
Parthjhaveri <parthjhaveri209@gmail.com>
2020-12-24 02:43:23 -0500

    added some comments to the dashboard component

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -9,10 +9,11 @@ const Dashboard = (props) => {
 
 	const [playing_track_name, set_playing_track_name] = useState(''); // CURRENTLY PLAYING SONG
 	const [playing_track_url, set_playing_track_url] = useState(''); // CURRENTLY PLAYING SONG
+	
 	const [previous_song_name, set_previous_song_name] = useState('');
 	const [previous_song, set_previous_song] = useState('');
 
-	// ON URL UPDATE
+	// ON CURRENT TRACK UPDATE, SET THE CURRENTLY PLAYING TRACK DATA TO STATE, KEEPING IT GENERIC
 	useEffect(() => {		
 		set_playing_track_name(props.current_track.track);		
 		set_playing_track_url(props.current_track.url);
@@ -21,19 +22,27 @@ const Dashboard = (props) => {
 	const show_previous = () => {	
 		console.log('PREVIOUSLY PLAYED SONGS ', props.previously_played);
 
+		// LOOP OVER THE ARRAY OF PREVIOUSLY PLAYED SONGS FROM THE END IN REVERSE ORDER
 		for (var i = props.previously_played.length - 1; i > 0; i--) {			
 
+			// IF THE CURRENT SONG PLAYING EQUALS THE SAME AS THE VERY LAST ELEMENT IN THE 
+			// 'PREVIOUSLY PLAYED' LOG, PLAY THE SONG RIGHT BEFORE IT (HENCE THE - 2)
 			if ((props.current_track.url.localeCompare(props.previously_played[i].url)) === 0) {
+
+				// SAVE THE PREVIOUS SONG URL IN STATE TO SET AS CURRENT LATER
 				set_previous_song(props.previously_played[props.previously_played.length - 2].url);
+
+				// SAVE THE PREVIOUS SONG NAME IN STATE TO SET AS CURRENT LATER
 				set_previous_song_name(props.previously_played[props.previously_played.length - 2].track);
 			}
 		}
 		
 	}
 
-	useEffect(() => {
-		console.log('PREVIOUS SONG URL ', previous_song);
-		console.log('PREVIOUS SONG name ', previous_song_name);
+	// WHEN THE 'PREV' BUTTON IS CLICKED, SET THE PREVIOUS SONG DATA = TO THE 
+	// CURRENTLY PLAYING SONG DATA, SO WE CAN USE THE GENERIC STATE VARIABLE TO PASS IN AS PROPS TO
+	// OUR CHILD COMPONENTS, ONE OF WHICH IS SOLELY PRESENTATIONAL (CONTROLS WIDGET)
+	useEffect(() => {		
 		set_playing_track_name(previous_song_name);		
 		set_playing_track_url(previous_song);
 	}, [previous_song]);
